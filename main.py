b6ccc1c8ff3dd15aa564c73dc2e98d2e53e9d900
lebibop <nikolaevklim@gmail.com>
2022-11-16 20:48:08 +0300

    hhh

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from math import *
 alpha = 1/3.793627  # coefficient for translation mm ---> px
 # (0,0) ---> (1920/2, 1080/2) new center (960, 540)
 
-for_round = 6
+for_round = 6 # for round float format .6
 
+
+def degree_2_radian(degree):
+    return (degree * pi) / 180
+def radian_2_degree(radian):
+    return (180 * radian) / pi
 
 class Camera:
     def __init__(self, lat, lon, alt, roll, pitch, yaw, px, py, f, m_a, m_b):
-        self.lat = lat  # latitude(deg)
-        self.lon = lon  # longitude(deg)
+        self.lat = degree_2_radian(lat)  # latitude(deg)
+        self.lon = degree_2_radian(lon)  # longitude(deg)
         self.alt = alt  # altitude(meters)
         self.roll = roll  # roll(degrees)
         self.pitch = pitch  # pitch(degrees)
@@ -19,16 +24,14 @@ class Camera:
         self.matrix_y = m_b  # matrix side, px
         self.px = px - matrix_x/2  # pixel X coordinate in the image, px
         self.py = (py - matrix_y/2) * (-1)  # pixel Y coordinate in the image, px
-
-
     def get_x_position_ideal(self):
-        if self.alt != 0 and self.px != 0:
+        if self.f != 0:
             return (self.px * alpha * self.alt)/(1000 * self.f)
         else:
             return 0
 
     def get_y_position_ideal(self):
-        if self.alt != 0 and self.py != 0:
+        if self.f != 0:
             return (self.py * alpha * self.alt)/(1000 * self.f)
         else:
             return 0
@@ -37,6 +40,7 @@ class Camera:
         return round(self.lat + self.get_x_position_ideal(), for_round), \
                round(self.lon + self.get_y_position_ideal(), for_round), \
                self.alt
+
 
 
 if __name__ == "__main__":
@@ -52,5 +56,9 @@ if __name__ == "__main__":
     yaw = 0
     # find coordinates in real life in ideal conditions
     camera = Camera(lat, lon, alt, roll, pitch, yaw, px, py, f, matrix_x, matrix_y)
- ##
+    print(camera.lat, camera.lon)
+    # дрон смотрит на строго север
+
+
+
     print(camera.get_coordinates_ideal())
